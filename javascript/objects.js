b774bde0888a8a4f79856081806de51bc83bd84e
Amber Lopata <amlopa16@stlawu.edu>
2020-02-03 13:07:18 -0500

    Javascript object literals

--- javascript/objects.js
+++ javascript/objects.js
@@ -3,3 +3,25 @@
 // Object literal -- basically like a python dictionary with methods
 const point = {x: 1, y: 2};
 console.log(point);
+
+// Accessing properties
+console.log(point.x);
+
+// Changing properties
+point.x = 3;
+
+// Nested Object literal
+const circle = {
+  center: point,
+  radues: 4
+};
+
+// Access subpropereties
+console.log(circle.center.x);
+
+// Undefined properties
+console.log(circle.color);
+
+// adding properties
+circle.color = 'blue';
+console.log(circle);
