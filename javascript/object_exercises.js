716ca626fbc334a743fd19ad3e81f8753b34467f
Amber Lopata <amlopa16@stlawu.edu>
2020-02-03 13:56:56 -0500

    change to 1

--- javascript/object_exercises.js
+++ javascript/object_exercises.js
@@ -1,15 +1,14 @@
 // Question 1
-const cs332 = {subject: 'CS', level: 332};
-const cs370 = {subject: 'CS', level: 370};
-const en245 = {subject: 'EN', level: 245};
-const courses = [cs332, cs370, en245];
 
-const get_course = function(courses){
+const get_course = function(){
+  const cs332 = {subject: 'CS', level: 332};
+  const cs370 = {subject: 'CS', level: 370};
+  const en245 = {subject: 'EN', level: 245};
+  const courses = [cs332, cs370, en245];
   let max = 0
   for (const course in courses) {
     console.log(course.level);
     if (course.level > max) {
-
       max = course
     }
   }
