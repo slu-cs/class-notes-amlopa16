c62a263805be3aae88a0574a9a235802b78574f1
Amber Lopata <amlopa16@stlawu.edu>
2020-02-05 13:44:59 -0500

    asynchronous and synchronous. user input

---
+++ javascript/input.js
@@ -0,0 +1,17 @@
+// Syntax  for console and file input
+
+// Synchronous calls - wait for a result, stuck until it arrives
+// Asynchronous calls - dont wait for a result, handle the result whenever it arrives
+
+const readline = require('readline');
+
+// console configuration
+const user = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// console input
+user.question('Enter a filename: ', function(filename)) {
+  
+});
