db70f628cb80cc9471dce9db819886fa5056cf4d
Amber Lopata <amlopa16@stlawu.edu>
2020-03-04 14:17:00 -0500

    3.4

--- express/bakery/pies.js
+++ express/bakery/pies.js
@@ -4,22 +4,22 @@ const express = require('express');
 // Create the Router
 const router = express.Router();
 
-// cs-linuxlab-21.stlawu.edu:3000/cakes/
+// cs-linuxlab-21.stlawu.edu:3000/pies/
 router.get('/', function(request, response) {
   response.send(`
     <h1>Pies</h1>
     <ul>
-      <li><a href="/cakes/vanilla">Vanilla</a></li>
-      <li><a href="/cakes/chocolate">Chocolate</a></li>
+      <li><a href="/cakes/apple">Apple</a></li>
+      <li><a href="/cakes/cherry">Cherry</a></li>
     </ul>
   `);
 });
 router.get('/:id', function(request, response, next){
-  if(request.params.id === 'vanilla') {
-    response.send('Vanilla cakes');
+  if(request.params.id === 'apple') {
+    response.send('Apple pies');
   }
-  else if(request.params.id === 'chocolate') {
-    response.send('Chocolate cakes');
+  else if(request.params.id === 'cherry') {
+    response.send('Cherry pies');
   }
   else{
     next(); // Pass on this request
